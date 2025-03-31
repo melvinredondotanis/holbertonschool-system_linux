5f926433e77ac90e2886c8058fc50c6e681febb7
Melvin Redondo--Tanis <melvin@redondotanis.com>
2025-03-31 15:10:48 +0200

    implement basic file listing functionality with string comparison

---
+++ ls/hls.h
@@ -0,0 +1,11 @@
+#ifndef HLS_H
+#define HLS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+/* Strings */
+int _strcmp(char *s1, char *s2);
+
+#endif /* HLS_H */
---
+++ ls/main.c
@@ -0,0 +1,27 @@
+#include "hls.h"
+
+/**
+ * main - Main function of hls
+ *
+ * Return: 0 on success, 1 on failure
+ */
+int main(void)
+{
+	DIR *dir;
+	struct dirent *read;
+
+	dir = opendir(".");
+	if (dir == NULL)
+		return (1);
+
+	while ((read = readdir(dir)) != NULL)
+	{
+		if (_strcmp(read->d_name, ".") == 0 || _strcmp(read->d_name, "..") == 0)
+			continue;
+		printf(" %s", read->d_name);
+	}
+	printf("\n");
+	closedir(dir);
+
+	return (0);
+}
