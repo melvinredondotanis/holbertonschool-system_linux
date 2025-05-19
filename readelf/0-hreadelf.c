a056a40357f02a9b5485999a74222b89528cbbd3
Melvin Redondo--Tanis <melvin@redondotanis.com>
2025-05-19 14:16:26 +0200

    feat: add readelf utility with multiple options and Makefile

---
+++ readelf/0-hreadelf.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ * @env: Environment variables
+ * Return: Always 0 (Success) or -1 (Failure)
+ */
+int main(int argc, char **argv, char **env)
+{
+	(void)argc;
+	char *av[] = {"/usr/bin/readelf", "-W", "-h", "", NULL};
+
+	av[3] = argv[1];
+	if (execve("/usr/bin/readelf", av, env) == -1)
+	{
+		perror("execv");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
