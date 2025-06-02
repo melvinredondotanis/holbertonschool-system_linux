d10581df706586e2364fc9c6107f253cea39c022
Melvin Redondo--Tanis <melvin@redondotanis.com>
2025-06-02 12:29:23 +0200

    feat: implement asm_strlen function and add main test file

---
+++ libasm/0-strlen.asm
@@ -0,0 +1,16 @@
+section .text
+	global asm_strlen	; Export the function
+
+asm_strlen:
+	xor rax, rax		; Init the RAX at 0 (I use xor, is faster than MOV in this case)
+
+.loop_count:
+	cmp byte [rdi], 0	; Verify if the current character is NULL
+	je .done			; If yes, it's done
+
+	inc rax				; Increment the RAX
+	inc rdi				; Move to the next character in the string
+	jmp .loop_count
+
+.done:
+	ret
---
+++ libasm/libasm.h
@@ -0,0 +1,11 @@
+#ifndef LIBASM_H
+#define LIBASM_H
+
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+size_t asm_strlen(const char *str);
+
+#endif /* LIBASM_H */
