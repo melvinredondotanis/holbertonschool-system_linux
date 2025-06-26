83e65c8de1714644717df74486703b05ecba11e8
Melvin Redondo--Tanis <melvin@redondotanis.com>
2025-06-26 10:50:16 +0200

    feat: implement red-black tree structure and print functionality

---
+++ red_black_tree/0-rb_tree_node.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "rb_trees.h"
+
+rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
+{
+
+	rb_tree_t *new_node = malloc(sizeof(rb_tree_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->color = color;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
+}
