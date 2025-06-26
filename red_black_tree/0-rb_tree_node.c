1b87f5e376bb8e932f65672ab61e55d84ba39eeb
Melvin Redondo--Tanis <melvin@redondotanis.com>
2025-06-26 10:53:48 +0200

    feat: enhance documentation for rb_tree_node function

--- red_black_tree/0-rb_tree_node.c
+++ red_black_tree/0-rb_tree_node.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include "rb_trees.h"
 
+/**
+ * rb_tree_node - Creates a new Red-Black tree node
+ * @parent: Pointer to the parent node
+ * @value: Integer value to be stored in the node
+ * @color: Color of the node (RED or BLACK)
+ *
+ * Return: Pointer to the newly created node, or NULL on failure
+ */
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 {
 
