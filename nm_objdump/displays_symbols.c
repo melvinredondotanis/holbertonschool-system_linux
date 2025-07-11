ba7083cd9a412ae41d904ac03fc9df42b89ecf39
Melvin Redondo--Tanis <melvin@redondotanis.com>
2025-07-11 10:12:13 +0200

    feat: implement file opening and error handling in displays_symbols function

--- nm_objdump/displays_symbols.c
+++ nm_objdump/displays_symbols.c
@@ -1,7 +1,30 @@
 #include "hnm.h"
 
-int displays_symbols(char *filename, char *program_name)
+static int open_file(char *filename, const char *program_name)
 {
-    printf("%s%s", filename, program_name);
-    return (EXIT_SUCCESS);
+	int fd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		if (errno == EACCES)
+			fflush(stdout), fprintf(stderr, ERR_NO_ACCESS, program_name, filename);
+		else if (errno == ENONET)
+			fflush(stdout), fprintf(stderr, ERR_NO_ENTRY, program_name, filename);
+	return (fd);
+}
+
+int displays_symbols(char *filename, const char *program_name)
+{
+	int status = EXIT_FAILURE, fd;
+	size_t r, num_printed = 0;
+	elf_t elf_header;
+
+	fd = open_file(filename, program_name);
+	if (fd == -1)
+		return (EXIT_FAILURE);
+
+	memset(&elf_header, 0, sizeof(elf_header));
+	r = read(fd, &elf_header.e64, sizeof(elf_header.e64));
+	if (r != sizeof(elf_header.e64) || tkt)
+		fprintf(stderr, ERR_NOT_MAGIC, program_name, filename);
 }
--- nm_objdump/hnm.h
+++ nm_objdump/hnm.h
@@ -12,6 +12,38 @@
 
 #define USAGE "Usage: hnm [objfile ...]\n"
 
-int displays_symbols(char *filename, char *program_name);
+#define ERR_PREFIX "%s: "
+#define ERR_NO_ENTRY ERR_PREFIX \
+	"'%s': No such file\n"
+#define ERR_NO_ACCESS ERR_PREFIX \
+	"%s: Permission denied\n"
+#define ERR_NOT_MAGIC ERR_PREFIX \
+	"%s: File format not recognized\n"
+
+/**
+ * struct Elf - stores 32/64 structs and other data
+ * @e64: the 64 bit struct
+ * @e32: the 32 bit struct
+ * @s64: the 64 bit struct section array
+ * @s32: the 32 bit struct section array
+ * @p64: the 64 bit struct program array
+ * @p32: the 32 bit struct program array
+ * @y64: the 64 bit struct symbol array
+ * @y32: the 32 bit struct symbol array
+ */
+typedef struct Elf
+{
+	Elf64_Ehdr e64;
+	Elf32_Ehdr e32;
+	Elf64_Shdr *s64;
+	Elf32_Shdr *s32;
+	Elf64_Phdr *p64;
+	Elf32_Phdr *p32;
+	Elf64_Sym *y64;
+	Elf32_Sym *y32;
+
+} elf_t;
+
+int displays_symbols(char *filename, const char *program_name);
 
 #endif /* HNM_H */
