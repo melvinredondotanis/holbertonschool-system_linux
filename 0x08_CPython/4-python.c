9d5319e7be55df5f3a6f3f028efb29ff5a48c23b
Melvin Redondo--Tanis <melvin@redondotanis.com>
2025-07-27 20:23:07 +0200

    feat: implement print_python_string function for Python object inspection

--- 0x08_CPython/4-python.c
+++ 0x08_CPython/4-python.c
@@ -8,16 +8,21 @@
  */
 void print_python_string(PyObject *p)
 {
-	wprintf(L"[.] string object info\n");
-	if (!p || p->ob_type != &PyUnicode_Type)
-	{
-		wprintf(L"  [ERROR] Invalid String Object\n");
-		return;
-	}
-	if (((PyASCIIObject *)p)->state.ascii)
-		wprintf(L"  type: compact ascii\n");
-	else
-		wprintf(L"  type: compact unicode object\n");
-	wprintf(L"  length: %ld\n", ((PyASCIIObject *)p)->length);
-	wprintf(L"  value: %ls\n", PyUnicode_AS_UNICODE(p));
+    wprintf(L"[.] string object info\n");
+    if (!p || !PyUnicode_Check(p))
+    {
+        wprintf(L"  [ERROR] Invalid String Object\n");
+        return;
+    }
+    if (((PyASCIIObject *)p)->state.ascii)
+        wprintf(L"  type: compact ascii\n");
+    else
+        wprintf(L"  type: compact unicode object\n");
+    wprintf(L"  length: %ld\n", ((PyASCIIObject *)p)->length);
+    wchar_t *value = PyUnicode_AsWideCharString(p, NULL);
+    if (value)
+    {
+        wprintf(L"  value: %ls\n", value);
+        PyMem_Free(value);
+    }
 }
