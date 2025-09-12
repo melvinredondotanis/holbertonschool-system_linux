8e711659b3aa5b30ceaab458eb0eaf2820830de1
Melvin Redondo--Tanis <melvin@redondotanis.com>
2025-09-12 14:55:02 +0200

    Refactor mutex handling in tprintf: add initialization and destruction functions

--- multithreading/20-tprintf.c
+++ multithreading/20-tprintf.c
@@ -1,8 +1,30 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 #include "multithreading.h"
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+void mutex_init(void) __attribute__((constructor));
+void mutex_destroy(void) __attribute__((destructor));
+static pthread_mutex_t mutex;
+
+/**
+ * mutex_init - ...
+ */
+void mutex_init(void)
+{
+	if (pthread_mutex_init(&mutex, NULL) != 0)
+		exit(EXIT_FAILURE);
+}
+
+/**
+ * mutex_destroy - ...
+ */
+void mutex_destroy(void)
+{
+	if (pthread_mutex_destroy(&mutex) != 0)
+		exit(EXIT_FAILURE);
+}
 
 /**
  * tprintf - ...
@@ -11,11 +33,19 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
  */
 int tprintf(char const *format, ...)
 {
-	pthread_mutex_lock(&mutex);
-	if (!format)
-		pthread_exit(&format);
+	int status = 0;
+	va_list args;
 
-	printf("[%lu] %s", pthread_self(), format);
+	if (!format)
+		return (-1);
+
+	pthread_mutex_lock(&mutex);
+
+	printf("[%lu] ", pthread_self());
+	va_start(args, format);
+	status = vprintf(format, args);
+	va_end(args);
+
 	pthread_mutex_unlock(&mutex);
-	return (0);
+	return (status);
 }
